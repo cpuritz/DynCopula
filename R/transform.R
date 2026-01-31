0ae8d9a63e5066b48e1434b316bc09ad8b9a1724
cpuritz <connorpuritz2025@u.northwestern.edu>
2026-01-31 13:14:17 -0600

    Comment update

--- R/transform.R
+++ R/transform.R
@@ -67,7 +67,6 @@ vec2cor <- function(v) {
 
     # Cap magnitude of correlations to rho_max
     max_R <- max(abs(copula::P2p(R)))
-
     if (max_R > rho_max) {
         alpha <- rho_max / max_R
         R <- alpha * R + (1 - alpha) * diag(dim(R)[1])
