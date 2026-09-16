19b1af44c3c6035f284f54a01a89fa702de535c2
cpuritz <connorpuritz2025@u.northwestern.edu>
2026-09-16 11:48:40 -0500

    Cleanup

---
+++ R/gamgc-package.R
@@ -0,0 +1,13 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom assertthat assert_that
+#' @importFrom S4Vectors metadata
+#' @importFrom S4Vectors metadata<-
+#' @importFrom stats quantile
+#' @importFrom testthat expect_error
+#' @importFrom testthat expect_identical
+#' @importFrom testthat test_that
+## usethis namespace: end
+NULL
